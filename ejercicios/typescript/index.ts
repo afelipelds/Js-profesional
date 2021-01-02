f959f9ea2647d34c59ebaf32e07a3ca501086500
afelipelds <afelipelds.ez@gmail.com>
2021-01-02 01:58:28 -0500

    Typescript - Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -3,5 +3,46 @@ console.log('Hello Typescript')
 function add(a: number, b:number) {
     return a + b
 }
+const sum = add(2,3);
+console.log('Saludo a todos con la función suma: ', sum);
 
-const sum = add(2,3)
+
+//Boolean
+let muted:boolean = false;
+
+
+//number
+let myNum: number = 21;
+
+
+//string
+const myLetter: string = `Mi edad es ${myNum}`;
+
+
+//array
+let people: string[] = [];
+people = ['Santiago', 'Rosalba', 'Felipe'];
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Andrés');
+peopleAndNumbers.push(999);
+
+console.log('people', people);
+console.log('peopleAndNumbers', peopleAndNumbers);
+
+//enum
+enum Color {
+    Rojo,
+    violeta,
+    Verde,
+}
+const COLORFAVORITO: Color = Color.Verde;
+console.log('color favorito es', COLORFAVORITO);
+
+//any
+let comodin: any = 'jOKER';
+comodin = {type: 'joker'};
+
+//object
+let myObj:object = {type: 'joker'};
+
